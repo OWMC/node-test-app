5664f373bd1b5f1c5404a59cdc62060135e7df27
Oliver Wieland <oliverwieland@Olivers-MacBook-Pro.local>
2024-09-01 13:04:40 +0100

    [NTA002] Fixed console log for fs module. And started emit method on new Events instance.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
+// Basic requester module with require/exports, and inbuilt modules path/os/fs
 
-var user = "Olly";
+// const path = require('path');
+// const os = require('os');
+// const fs = require('fs');
+// 
+// var user = "Olly";
+// 
+// function sayHello(name) {
+//     console.log(`Good day to you, ${name}!`);
+// };
+// sayHello(user);
+// 
+// console.log("You are using: ", path.parse(__filename).name);
+// 
+// // console.log("OS total memory: ", os.totalmem());
+// console.log("OS free memory:  ", os.freemem());
+// 
+// fs.readdir('./', (err, files) => {
+//     if (err) console.log('Files in app... ERROR!  ', err);
+//     else console.log("Files in app: ", files);
+// });
+//
+// const requester = require('./requester');
+// 
+// requester(user);
 
-function sayHello(name) {
-    console.log(`Good day to you, ${name}!`);
-};
-sayHello(user);
+// Event module, class: EventEmitter
 
-console.log("You are using: ", path.parse(__filename).name);
+const EventEmitter = require('events'); // Class - First letter is uppercase when referencing a Class (a container for props and methods, not a function).
+const emitter = new EventEmitter(); // Object - an instance of the class
 
-console.log("OS total memory: ", os.totalmem());
-console.log("OS free memory:  ", os.freemem());
-
-console.log('Files in app: ' + fs.readdir('./', (err, files) => {
-    if (err) console.log('Error: ', err);
-    else console.log("Result: ", files);
-}));
-
-
-const requester = require('./requester');
-
-requester(user);
+emitter.on('messageLogged', () => {
+    console.log("Listener called: ");
+}); // on is like addListener
+emitter.emit('messageLogged');
